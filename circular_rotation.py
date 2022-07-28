7cb81b58448b4b5e002720cb75d5b59dfb1990cd
bk263_vishal_igms <69063363+vishallog@users.noreply.github.com>
2022-07-28 14:16:50 +0530

    Update circular_rotation.py

--- circular_rotation.py
+++ circular_rotation.py
@@ -1,15 +1,43 @@
-arr = [1,2,3,4,5,6]
-n = len(arr)
-d = 2
-c = 0
-while c < d:
-    x = arr[n-1]
-    for i in range(n-1,0,-1):
-        arr[i] = arr[i - 1]
-    arr[0] = x
-    c += 1
+#|RIGHT| rotation--------------------------------
+def leftrotate(arr, d, n):
+    c = 0
+    while c < d:
+        x = arr[n-1]
+        for i in range(n-1,0,-1):
+            arr[i] = arr[i - 1]
+        arr[0] = x
+        c += 1
+    return arr
+if __name__ == '__main__':
+    arr = [1,2,3,4,5,6]
+    N = len(arr)
+    d = 2
+    c = 0
+    arr = leftrotate(arr, d, N)
+    for i in arr:
+        print(i, end=" ")
 
-for i in arr:
-    print(i, end=" ")
+        
+ #|LEFT| rotation-------------------------------------------------------------------------------------
+
+def leftrotate(arr, d, n):
+    c = 0
+    while c < d:
+        x = arr[0]
+        for i in range(0,n-1,1):
+            arr[i] = arr[i + 1]
+        arr[n-1] = x
+        c += 1
+    return arr
+if __name__ == '__main__':
+    arr = [1,2,3,4,5,6]
+    N = len(arr)
+    d = 2
+    c = 0
+    arr = leftrotate(arr, d, N)
+    for i in arr:
+        print(i, end=" ")
+
+
 
 
